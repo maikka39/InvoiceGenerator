22f7cf7b39d73c67549c666dec7725c95af52c4a
Maik de Kruif <maikka39@gmail.com>
2019-03-31 21:23:46 +0200

    Add the request date in text

--- frontend/js/start_screen.js
+++ frontend/js/start_screen.js
@@ -6,6 +6,22 @@ const {
 // Load the values script
 const values = remote.require('./backend/values');
 
+// Dictonary to convert month to Dutch string
+let months = {
+  1: "januari",
+  2: "februari",
+  3: "maart",
+  4: "april",
+  5: "mei",
+  6: "juni",
+  7: "juli",
+  8: "augustus",
+  9: "september",
+  10: "oktober",
+  11: "november",
+  12: "december"
+};
+
 /**
  * Save data and go to the next screen.
  */
@@ -27,6 +43,8 @@ function nextScreen() {
   values.set('project_title', projectTitle);
   values.set('project_number', projectNumber);
   values.set('request_date', requestDate);
+
+  values.set('request_date_text', parseInt(requestDate.split("-")[2]).toString() + months[parseInt(requestDate.split("-")[1])] + requestDate.split("-")[0]);
 
   // Redirect the user to the next page
   window.location.href = "template.html";
